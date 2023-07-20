af3d187cdd433e6e1e56daff6fc2f92944b2ef4f
Tim Quattrochi <timq82@gmail.com>
2023-07-20 18:53:11 -0400

    added server dependancies, added in middleware to server.js

--- server/server.js
+++ server/server.js
@@ -2,19 +2,27 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const path = require("path");
 const cookieParser = require("cookie-parser");
 const { PORT, NODE_ENV, API_URL } = require("./config/constants");
 const connectDB = require("./config/db");
 const userRouter = require("./routes/auth.routes");
+const { logger } = require("./middleware/logger");
+const { AppError } = require("./middleware/AppError");
+const credentials = require("./middleware/credentials");
+const corsOptions = require("./config/corsOptions");
 
 const app = express();
 
 //Middlewares
-app.use(cors());
+app.use(cors(corsOptions));
+app.use(morgan("dev")); //logging
+app.use(credentials);
+app.use(logger);
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev")); //logging
+app.use(express.static(path.join(__dirname, "public")));
 
 //DB connection
 connectDB();
@@ -32,6 +40,9 @@ if (NODE_ENV === "production") {
     );
   });
 }
+
+//error handler middlemare.
+app.use(AppError);
 
 app.listen(PORT, (err) => {
   if (err) {
