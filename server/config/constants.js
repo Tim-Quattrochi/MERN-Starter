c172f310109d19d3e3ebcaf317e83e2e86f8eb9b
Tim Quattrochi <timq82@gmail.com>
2023-07-20 11:16:59 -0400

    added some fallbacks to constants.

--- server/config/constants.js
+++ server/config/constants.js
@@ -1,13 +1,12 @@
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 const NODE_ENV = process.env.NODE_ENV;
 const DB_URI = process.env.DB_URI;
-const API_URL = process.env.API_URL;
+const API_URL = process.env.API_URL || "/api";
 const JWT_SECRET = process.env.JWT_SECRET;
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
-const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
-const REFRESH_EXPIRES_IN = process.env.REFRESH_EXPIRES_IN;
-const APP_NAME = process.env.APP_NAME;
-
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "1h";
+const REFRESH_EXPIRES_IN = process.env.REFRESH_EXPIRES_IN || "10d";
+const APP_NAME = process.env.APP_NAME || "Mern Starter App";
 
 module.exports = {
   PORT,
@@ -17,6 +16,6 @@ module.exports = {
   JWT_SECRET,
   REFRESH_EXPIRES_IN,
   REFRESH_TOKEN_SECRET,
-    JWT_EXPIRES_IN,
-  APP_NAME
+  JWT_EXPIRES_IN,
+  APP_NAME,
 };
