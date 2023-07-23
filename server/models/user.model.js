9bb2bcffe937526a7bf6e793d6d045f60c2a0550
Tim Quattrochi <timq82@gmail.com>
2023-07-23 02:21:11 -0400

    email validation on mongoose user schema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,21 +1,26 @@
 const mongoose = require("mongoose");
+const { AppError } = require("../middleware/AppError");
 
-const userSchema = mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
       required: [true, "Please enter a name."],
     },
     email: {
-        type: String,
-        unique: true,
+      type: String,
+      unique: true,
+      lowercase: true,
       required: [true, "Please enter an email."],
+      match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
     },
 
     password: {
       type: String,
       required: [true, "Please enter a password."],
+      minlength: 8,
     },
+
     refreshToken: {
       type: String,
     },
