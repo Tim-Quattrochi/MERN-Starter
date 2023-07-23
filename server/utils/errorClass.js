efe2cd12094d0e614406adc10ef84d1e7b1f759c
Tim Quattrochi <timq82@gmail.com>
2023-07-23 02:22:17 -0400

    capture the stack trace on CustomError class

--- server/utils/errorClass.js
+++ server/utils/errorClass.js
@@ -1,8 +1,10 @@
 class CustomError extends Error {
   constructor(message, statusCode) {
     super(message);
+
     this.name = this.constructor.name;
     this.statusCode = statusCode;
+    Error.captureStackTrace(this, this.constructor);
   }
 }
 
