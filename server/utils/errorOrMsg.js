fa658c9caa37e3fbd0fc29709882eeb64e658464
Tim Quattrochi <timq82@gmail.com>
2023-07-28 20:40:42 -0400

    added jsdoc to errorOrMsg utility function

--- server/utils/errorOrMsg.js
+++ server/utils/errorOrMsg.js
@@ -1,3 +1,10 @@
+/**
+ * @description - Utility function that determines whether to send
+ * an error or message property in response messages to the client.
+ * @param {number} statusNum - http status code
+ * @param {string} message - the message you want to response with.
+ * @returns string - message or error depending on the status number.
+ */
 const errorOrMsg = (statusNum, message) => {
   const property = statusNum <= 299 ? "message" : "error";
 
