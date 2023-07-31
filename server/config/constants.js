ab3301c41ff6c14a92f0595b875bb8ca8f05d819
Tim Quattrochi <timq82@gmail.com>
2023-07-31 19:18:42 -0400

    changed JWT_SECRET and JWT_EXPIRES_IN to ACCESS, since it is the accessToken secrets

--- server/config/constants.js
+++ server/config/constants.js
@@ -2,9 +2,9 @@ const PORT = process.env.PORT || 3001;
 const NODE_ENV = process.env.NODE_ENV;
 const DB_URI = process.env.DB_URI;
 const API_URL = process.env.API_URL || "/api";
-const JWT_SECRET = process.env.JWT_SECRET;
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
-const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "1h";
+const ACCESS_EXPIRES_IN = process.env.ACCESS_EXPIRES_IN || "1h";
 const REFRESH_EXPIRES_IN = process.env.REFRESH_EXPIRES_IN || "10d";
 const APP_NAME = process.env.APP_NAME || "Mern Starter App";
 
@@ -13,9 +13,9 @@ module.exports = {
   NODE_ENV,
   DB_URI,
   API_URL,
-  JWT_SECRET,
+  ACCESS_TOKEN_SECRET,
+  ACCESS_EXPIRES_IN,
   REFRESH_EXPIRES_IN,
   REFRESH_TOKEN_SECRET,
-  JWT_EXPIRES_IN,
   APP_NAME,
 };
