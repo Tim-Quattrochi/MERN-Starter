0acab80a1f4bd26b1d9447e8dc31f0dff4334149
Tim Quattrochi <timq82@gmail.com>
2023-08-04 23:57:00 -0400

    took out case for REGISTER as it was redundant

--- client/src/context/authReducer.js
+++ client/src/context/authReducer.js
@@ -6,16 +6,17 @@ export const initialState = {
   errorMsg: null,
 };
 
+/**
+ *
+ * @param {Object} state - the current auth state
+ * @param {Object} action -The action object containing the type and payload.
+ * @returns - the updated state object based on the action type
+ * provided
+ */
+
 export const authReducer = (state, action) => {
   switch (action.type) {
     case "LOGIN":
-      return {
-        ...state,
-        isAuthenticated: true,
-        user: action.payload.user,
-        accessToken: action.payload.accessToken,
-      };
-    case "REGISTER":
       return {
         ...state,
         isAuthenticated: true,
