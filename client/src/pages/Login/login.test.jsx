1c077627dba4ef5bdab47bd0a3f9e776b163a87b
Tim Quattrochi <timq82@gmail.com>
2023-10-06 21:44:53 -0400

    updated login test

--- client/src/pages/Login/login.test.jsx
+++ client/src/pages/Login/login.test.jsx
@@ -19,31 +19,94 @@ const renderLogin = () => {
   );
 };
 
+//elements to be tested,  organized in a function.
+/**
+
+ * @description - returns the elements to be tested
+ * @returns {Object} - returns an object with the elements to be tested
+ * @example renderInputs().emailInput - returns the email input
+ */
+const renderInputs = () => {
+  const emailInput = screen.getByLabelText(/email/i);
+  const passwordInput = screen.getByLabelText(/password/i);
+  const submitBtn = screen.getByRole("button", { name: /submit/i });
+  const link = screen.getByRole("link", {
+    name: /need an account\?/i,
+  });
+
+  return { emailInput, passwordInput, submitBtn, link };
+};
+
 describe("login test suite", () => {
   it("it tests that there are 2 inputs and a button", async () => {
     renderLogin();
-    //elements
-    const emailInput = screen.getByLabelText("Email");
-    const passwordInput = screen.getByLabelText("Password");
-    const submitBtn = screen.getByRole("button", { name: /submit/i });
 
     //assertions
-    expect(emailInput).toBeInTheDocument();
-    expect(passwordInput).toBeInTheDocument();
-    expect(submitBtn).toBeInTheDocument();
+    expect(renderInputs().emailInput).toBeInTheDocument();
+    expect(renderInputs().passwordInput).toBeInTheDocument();
+    expect(renderInputs().submitBtn).toBeInTheDocument();
   });
 
   it("it tests that clicking on need an account link navigates to the register page", async () => {
     renderLogin();
 
-    const link = screen.getByRole("link", {
-      name: /need an account\?/i,
-    });
-
-    fireEvent.click(link);
+    fireEvent.click(renderInputs().link);
 
     await waitFor(() => {
       expect(window.location.pathname).toBe("/register");
+    });
+  });
+});
+
+describe("login form validation", () => {
+  it("it tests that the email input is required", async () => {
+    renderLogin();
+
+    fireEvent.click(renderInputs().submitBtn);
+
+    await waitFor(() => {
+      expect(
+        screen.getByText(/email is required\./i)
+      ).toBeInTheDocument();
+    });
+
+    fireEvent.change(renderInputs().emailInput, {
+      target: { value: "test" },
+    });
+
+    await waitFor(() => {
+      expect(
+        screen.getByText("Email is required.")
+      ).toBeInTheDocument();
+    });
+  });
+
+  it("it tests that the password input is required", async () => {
+    renderLogin();
+
+    fireEvent.click(renderInputs().submitBtn);
+
+    await waitFor(() => {
+      expect(
+        screen.getByText("Password is required.")
+      ).toBeInTheDocument();
+    });
+  });
+  it("tests that the password must be at least 7 characters long", async () => {
+    renderLogin();
+
+    fireEvent.change(renderInputs().passwordInput, {
+      target: { value: "short" },
+    });
+
+    fireEvent.click(renderInputs().submitBtn);
+
+    await waitFor(() => {
+      expect(
+        screen.getByText(
+          "Password must be at least 7 characters long."
+        )
+      ).toBeInTheDocument();
     });
   });
 });
