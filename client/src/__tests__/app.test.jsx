6c7c6d140b23a8a398e6efea265b30934bed5483
Tim Quattrochi <timq82@gmail.com>
2024-01-20 16:06:26 -0500

    fixed failing reg/log test

--- client/src/__tests__/app.test.jsx
+++ client/src/__tests__/app.test.jsx
@@ -1,4 +1,5 @@
 import App from "../App.jsx";
+import { AuthProvider } from "../hooks/useAuthProvider.jsx";
 import { MemoryRouter } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
@@ -6,7 +7,9 @@ import { describe, it, expect } from "vitest";
 const renderPath = (path) => {
   render(
     <MemoryRouter initialEntries={[`/${path}`]}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </MemoryRouter>
   );
 };
@@ -21,11 +24,18 @@ describe("App.jsx routing test suite", () => {
 
   it("it tests that the login component is rendered at path /login", () => {
     renderPath("login");
-    expect(screen.getByText("Login")).toBeInTheDocument();
+    const loginText = screen.getAllByText(/login/i);
+
+    expect(loginText).toHaveLength(2);
+    expect(loginText[0].textContent).toBe("Login");
   });
 
   it("it tests that the register component is rendered at path /register", () => {
     renderPath("register");
-    expect(screen.getByText("Register")).toBeInTheDocument();
+
+    const registerText = screen.getAllByText(/register/i);
+
+    expect(registerText).toHaveLength(2);
+    expect(registerText[0].textContent).toBe("Register");
   });
 });
