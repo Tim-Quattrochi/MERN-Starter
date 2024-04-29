10267b843e4c2a032a51956e4dc8beffc27a6d36
Tim Quattrochi <timq82@gmail.com>
2024-04-28 21:22:06 -0400

    add prettier/recommended to estlintrc

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
   ],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
